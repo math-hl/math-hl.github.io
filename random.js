1a0dceb8b1118d59788127cd07bbc3f1a1dca851
George Valtas <grvaltas@gmail.com>
2019-03-09 21:42:42 +0200

    Add files via upload

--- random.js
+++ random.js
@@ -1,42 +1,19 @@
 random_btn.onclick = function() {
-
-	var questionAndAnswers = [
-		{
-			"questionUrl": "images/complex_numbers/1q.png",
-			"answerUrl": "images/complex_numbers/1ms.png"
-		},
-		{
-			"questionUrl": "images/complex_numbers/2q.png",
-			"answerUrl": "images/complex_numbers/2ms.png"
-		},
-		{
-			"questionUrl": "images/math_induction/1q.png",
-			"answerUrl": "images/math_induction/1ms.png"
-		},
-		{
-			"questionUrl": "images/math_induction/2q.png",
-			"answerUrl": "images/math_induction/2ms.png"
-		},
-		{
-			"questionUrl": "images/trigonometry/1q.png",
-			"answerUrl": "images/trigonometry/1ms.png"
-		},
-		{
-			"questionUrl": "images/trigonometry/2q.png",
-			"answerUrl": "images/trigonometry/2ms.png"
-		},
-	];
-
 	// generate a random number then round it down
-	var randomQuestionNumber = Math.floor(Math.random() * (questionAndAnswers.length + 1));
-	img_answer.style.visibility = "hidden";
+	var random = Math.floor(Math.random()*5)
+	console.log(random)
+	// the topic which will be chosen randomly too
+	var topics = [" ", "series_induction", "functions", "complex_numbers", "calculus", "logarithms", "statistics_probability", "trigonometry", "vectors"]
+	topicsRandom = Math.ceil(Math.random()*8)
+	console.log(topicsRandom)
+	// you will have to spam the "random" button before it works properly
 	img_question.style.visibility = "visible";
-	img_question.src = questionAndAnswers[randomQuestionNumber].questionUrl;
+	img_question.src = "images/" + topics[topicsRandom] + "/" + random + "q.png"
 	if (img_question.style.visibility == "visible") {
 		answer_btn.style.opacity = "1"
 		answer_btn.onclick = function() {
 			img_answer.style.visibility = "visible";
-			img_answer.src = questionAndAnswers[randomQuestionNumber].answerUrl;
+			img_answer.src = "images/" + topics[topicsRandom] + "/" + random + "ms.png"			
 		}
 	}
 }
